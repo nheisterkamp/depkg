602985102f1415657159015f493b60cf1ce86f37
Niels Heisterkamp <niels@governance.com>
2020-10-07 10:28:08 +0200

    v0.0.2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const fs = require('fs-extra')
 const path = require('path')
+const pkg = require('./package.json')
+
+console.log(`${pkg.name} v${pkg.version}`)
 
 const usage = () => {
   console.log(`Usage: depkg <INPUT> [OUTPUT]`)
@@ -40,18 +43,16 @@ const main = async () => {
   const vfs = JSON.parse('{"/snapshot/' + prelude.toString().match(EXTRACT_VFS)[1]);
 
   for (const name of Object.keys(vfs)) {
-    if (vfs[name]['0']) {
-      if (!vfs[name]['1']) {
-        console.log(`Could not extract ${name}`)
-      } else {
-        const [ position, size ] = vfs[name]['1']
-        const content = payload.slice(position, position + size)
-  
-        const nameWithoutSnapshot = name.replace(/^\/snapshot\//, '')
-        console.log(`Extracting ${nameWithoutSnapshot}`)
-        await fs.mkdirp(path.join(OUTPUT_FOLDER, path.dirname(nameWithoutSnapshot)))
-        await fs.writeFile(path.join(OUTPUT_FOLDER, nameWithoutSnapshot), content, 'utf-8')
-      }
+    if (!vfs[name]['1']) {
+      console.log(`Could not extract ${name}`)
+    } else {
+      const [ position, size ] = vfs[name]['1']
+      const content = payload.slice(position, position + size)
+
+      const nameWithoutSnapshot = name.replace(/^\/snapshot\//, '')
+      console.log(`Extracting ${nameWithoutSnapshot}`)
+      await fs.mkdirp(path.join(OUTPUT_FOLDER, path.dirname(nameWithoutSnapshot)))
+      await fs.writeFile(path.join(OUTPUT_FOLDER, nameWithoutSnapshot), content, 'utf-8')
     }
   }
 }
